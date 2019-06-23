fa94162cec247d8767cc81d4434b4e3fd3fd7912
Sanchit Ahuja <sanchit.ahuja@innovaccer.com>
2019-06-23 20:54:06 +0530

    Updates

---
+++ precache-manifest.af5c032294d08b9d53e646661e991f66.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e6b715a2d9de1c50bdee114e140a37",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "362f978d26df09c47afc",
+    "url": "/Shopping-Cart/static/css/main.048fc1d6.chunk.css"
+  },
+  {
+    "revision": "03d0ece2dabc75f73426",
+    "url": "/Shopping-Cart/static/js/2.9b9a2311.chunk.js"
+  },
+  {
+    "revision": "362f978d26df09c47afc",
+    "url": "/Shopping-Cart/static/js/main.be3b26f7.chunk.js"
+  },
+  {
+    "revision": "fefe738030062c2c7754",
+    "url": "/Shopping-Cart/static/js/runtime~main.9ff5cdc7.js"
+  },
+  {
+    "revision": "15ce06f6536e9dabd51fc564b6561ecf",
+    "url": "/Shopping-Cart/static/media/apple.15ce06f6.jpg"
+  },
+  {
+    "revision": "be6b6ed795beb2d2fd0f0ceca5546c5e",
+    "url": "/Shopping-Cart/static/media/banana.be6b6ed7.jpg"
+  },
+  {
+    "revision": "7451368287448bbbf20fff7209006f12",
+    "url": "/Shopping-Cart/static/media/grapes.74513682.jpg"
+  },
+  {
+    "revision": "af6f349006a12b3a9c27665f617ba0f7",
+    "url": "/Shopping-Cart/static/media/mango.af6f3490.jpg"
+  },
+  {
+    "revision": "75d5612e33b489f8ea1e7212c7309290",
+    "url": "/Shopping-Cart/static/media/orange.75d5612e.jpg"
+  },
+  {
+    "revision": "4af1474580a72c1ad7b03302b2e43ed3",
+    "url": "/Shopping-Cart/static/media/papaya.4af14745.jpg"
+  },
+  {
+    "revision": "257fa5275e6c4f6ce47e797baf60c08b",
+    "url": "/Shopping-Cart/static/media/watermelon.257fa527.jpg"
+  }
+]);
